6d04ebff9ab711fc49064640ed34266d01f0929b
Panov Alexey <alexeykapanov@gmail.com>
2020-08-04 14:33:25 +0300

    Create src section for application. Create blank for application service provider.

---
+++ src/ServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace CodeblogPro\GeoLocation;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    public function register()
+    {
+        parent::register();
+    }
+
+    public function boot()
+    {
+    }
+ }
