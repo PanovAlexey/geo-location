ffc3c7e57f7a56d33eb98e491cd72ec911726033
Panov Alexey <alexeykapanov@gmail.com>
2020-08-08 15:20:24 +0300

    Add postal, country iso and region iso fields to LocationDTO.

--- src/Application/Models/LocationDTO.php
+++ src/Application/Models/LocationDTO.php
@@ -10,13 +10,26 @@ class LocationDTO implements LocationInterface
     private string $country;
     private string $region;
     private string $city;
+    private string $postal;
+    private string $countryIso;
+    private string $regionIso;
     private CoordinatesInterface $coordinates;
 
-    public function __construct(string $country, string $region, string $city, CoordinatesInterface $coordinates)
-    {
+    public function __construct(
+        string $country,
+        string $region,
+        string $city,
+        string $postal,
+        string $countryIso,
+        string $regionIso,
+        CoordinatesInterface $coordinates
+    ) {
         $this->country = $country;
         $this->region = $region;
         $this->city = $city;
+        $this->postal = $postal;
+        $this->countryIso = $countryIso;
+        $this->regionIso = $regionIso;
         $this->coordinates = $coordinates;
     }
 
@@ -33,6 +46,21 @@ class LocationDTO implements LocationInterface
     public function getCity(): string
     {
         return $this->city;
+    }
+
+    public function getPostal(): string
+    {
+        return $this->postal;
+    }
+
+    public function getCountryIso(): string
+    {
+        return $this->countryIso;
+    }
+
+    public function getRegionIso(): string
+    {
+        return $this->regionIso;
     }
 
     public function getCoordinates(): CoordinatesInterface
