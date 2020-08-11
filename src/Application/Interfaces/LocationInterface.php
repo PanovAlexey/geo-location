d44c20235fa0678472df2acab4e8c30a7f7bfbef
Panov Alexey <alexeykapanov@gmail.com>
2020-08-11 19:20:49 +0300

    Add getters-methods for other fields of LocationInterface.

--- src/Application/Interfaces/LocationInterface.php
+++ src/Application/Interfaces/LocationInterface.php
@@ -2,10 +2,6 @@
 
 namespace CodeblogPro\GeoLocation\Application\Interfaces;
 
-/**
- * Interface LocationInterface
- * @package CodeblogPro\GeoLocation\Application\Interfaces
- */
 interface LocationInterface
 {
     public function getCountry(): string;
@@ -14,5 +10,13 @@ interface LocationInterface
 
     public function getCity(): string;
 
+    public function getPostal(): string;
+
+    public function getCountryIso(): string;
+
+    public function getRegionIso(): string;
+
     public function getCoordinates(): ?CoordinatesInterface;
+
+    public function toArray(): array;
 }
