0a420836245efc41f646cb34a9917c3a25f13047
Panov Alexey <alexeykapanov@gmail.com>
2020-08-11 22:02:01 +0300

    Add dynamic language detection from the URL.

--- src/Application/Models/Language.php
+++ src/Application/Models/Language.php
@@ -2,7 +2,6 @@
 
 namespace CodeblogPro\GeoLocation\Application\Models;
 
-use CodeblogPro\GeoLocation\Application\Enums\AvailableLanguages;
 use CodeblogPro\GeoLocation\Application\Interfaces\LanguageInterface;
 use CodeblogPro\GeoLocation\Application\Exceptions\IncorrectLanguageCodeException;
 
@@ -23,11 +22,14 @@ class Language implements LanguageInterface
     private function validate(string $code): string
     {
         $code = mb_strtoupper(trim($code));
+        $validatedCode = '';
 
-        if (!isset(AvailableLanguages::$code)) {
-            throw new IncorrectLanguageCodeException($code);
+        try {
+            $validatedCode = constant('\CodeblogPro\GeoLocation\Application\Enums\AvailableLanguages::' . $code);
+        } catch (\Exception $exception) {
+            throw new IncorrectLanguageCodeException();
         }
 
-        return AvailableLanguages::$code;
+        return $validatedCode;
     }
 }
--- src/routes/geo-location.php
+++ src/routes/geo-location.php
@@ -3,8 +3,8 @@ use Illuminate\Support\Facades\Route;
 use CodeblogPro\GeoLocation\Controller\GeoLocationController;
 
 Route::prefix('api')->middleware('api')->group(function() {
-    Route::get('/geo-location/{ip?}', [GeoLocationController::class, 'show'])->name('codeblogpro.geolocation.get.location');
-    Route::match(['post', 'put', 'patch', 'delete', 'options'], '/geo-location/{ip?}', [
+    Route::get('/geo-location/{ip?}/{language?}', [GeoLocationController::class, 'show'])->name('codeblogpro.geolocation.get.location');
+    Route::match(['post', 'put', 'patch', 'delete', 'options'], '/geo-location/{ip?}/{language?}', [
         GeoLocationController::class, 'incorrectMethod']
     );
 });
